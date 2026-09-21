c33f0f60ab5b5702d586fc6e0abcca7506894634
imbanik69-alt <imbanik69@gmail.com>
2026-09-21 12:42:25 +0530

    Update solution.py

--- Q15-Tree-Height/solution.c
+++ Q15-Tree-Height/solution.c
@@ -1,19 +1,26 @@
-﻿#include<stdio.h>
-#include<stdlib.h>
-struct Node{int data;struct Node*left,*right;};
-struct Node* newNode(int d){
-    struct Node*n=(struct Node*)malloc(sizeof(struct Node));
-    n->data=d;n->left=n->right=NULL;return n;
-}
-int height(struct Node*root){
-    if(!root) return 0;
-    int l=height(root->left),r=height(root->right);
-    return 1+(l>r?l:r);
-}
-int main(){
-    struct Node*root=newNode(1);
-    root->left=newNode(2);root->right=newNode(3);
-    root->left->left=newNode(4);
-    printf("Height = %d\n",height(root));
-    return 0;
-}
+﻿class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def height(root):
+    if root is None:
+        return 0
+
+    l = height(root.left)
+    r = height(root.right)
+
+    return 1 + max(l, r)
+
+
+# Creating the tree
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+
+print("Height =", height(root))
